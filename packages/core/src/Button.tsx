2721112ba57ab5824635d856861016d0dfba6ef0
Anto Subash <antosubash@live.com>
2022-09-30 18:34:11 +0200

    Update Button.tsx

--- packages/core/src/Button.tsx
+++ packages/core/src/Button.tsx
@@ -5,7 +5,7 @@ export interface ButtonProps {
 }
 
 export function Button(props: ButtonProps) {
-  return <button>{props.children}</button>;
+  return <button className="bg-white p-3 text-lg font-bold">{props.children}</button>;
 }
 
 Button.displayName = "Button";
