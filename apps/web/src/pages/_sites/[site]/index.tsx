907224a9318c6f47cdb3367b0ba5711ecc420477
Anto Subash <antosubash@live.com>
2022-11-07 00:15:44 +0100

    Update index.tsx

--- apps/web/src/pages/_sites/[site]/index.tsx
+++ apps/web/src/pages/_sites/[site]/index.tsx
@@ -7,7 +7,7 @@ export default function Home( { site } : any) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Web - Turborepo Example</title>
+        <title>Web - Example</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
