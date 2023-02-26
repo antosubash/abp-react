e8b0e36d4767b9df802c0843533ac4f8887c4346
Anto Subash <antosubash@live.com>
2023-02-26 18:59:16 +0100

    docker maybe working

--- abpreact/apps/abp/next.config.js
+++ abpreact/apps/abp/next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 module.exports = {
   reactStrictMode: true,
-  output: "standalone"
+  // transpilePackages: ['@abpreact/ui', '@abpreact/hooks', '@abpreact/utils', '@abpreact/proxy' ],
+  output: "standalone",
+  experimental: {
+    // this includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(__dirname, '../../'),
+  },
 }
---
+++ abpreact/clean.bat
@@ -0,0 +1,15 @@
+@ECHO off
+cls
+
+ECHO Deleting all node_modules,.dist,.turbo folders...
+ECHO.
+
+FOR /d /r . %%d in (node_modules,.dist,.turbo,out) DO (
+	IF EXIST "%%d" (		 	 
+		ECHO.Deleting: %%d
+			rd /s/q "%%d"
+	)
+)
+
+ECHO node_modules,.dist,.turbo folders have been successfully deleted. Press any key to exit.
+pause > nul
