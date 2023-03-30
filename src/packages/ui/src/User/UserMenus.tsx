7a1b7aeafe7cc91b18ebab3a977c210074012ab5
sajanv88 <work@sajankumarv.com>
2023-03-30 18:05:43 +0200

    updated button theme

--- src/packages/ui/src/User/UserMenus.tsx
+++ src/packages/ui/src/User/UserMenus.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { UserDropDown } from './UserDropDown';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import { getCookie } from 'cookies-next';
+import { Button } from '../Shared/Button';
 export interface UserMenusProps {}
 
 export const UserMenus = ({}: UserMenusProps) => {
@@ -12,9 +13,9 @@ export const UserMenus = ({}: UserMenusProps) => {
         }
 
         return (
-            <div>
-                <button
-                    className="mr-6 hover:bg-slate-300  p-3 rounded-xl"
+            <div className="space-x-2">
+                <Button
+                    variant="subtle"
                     onClick={() => {
                         signIn('openiddict', undefined, {
                             __tenant: getCookie('__tenant') as string
@@ -23,15 +24,15 @@ export const UserMenus = ({}: UserMenusProps) => {
                     }}
                 >
                     Login
-                </button>
-                <button
-                    className="py-2 px-4 text-white bg-black rounded-3xl"
+                </Button>
+                <Button
+                    variant="subtle"
                     onClick={() =>
                         (location.href = `${process.env.NEXT_PUBLIC_API_URL}/Account/Register`)
                     }
                 >
                     Register
-                </button>
+                </Button>
             </div>
         );
     };
