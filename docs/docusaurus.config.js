daa1794105d385feb83eb2fb4bfc3629cc951467
Anto Subash <antosubash@live.com>
2023-04-02 23:59:48 +0200

    updated docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -122,12 +122,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/antosubash/abpreact',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Anto Subash. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
