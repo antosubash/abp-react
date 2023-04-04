8e5547e0f06b40f330aeca645acae8f1e3c3190e
Anto Subash <antosubash@live.com>
2023-04-04 23:45:02 +0200

    added nuget

--- src/clean.bat
+++ src/clean.bat
@@ -4,8 +4,8 @@ cls
 ECHO Deleting all node_modules,.dist,.turbo folders...
 ECHO.
 
-FOR /d /r . %%d in (node_modules,.dist,.turbo,out) DO (
-	IF EXIST "%%d" (		 	 
+FOR /d /r . %%d in (node_modules,dist,.turbo,out,.next) DO (
+	IF EXIST "%%d" (
 		ECHO.Deleting: %%d
 			rd /s/q "%%d"
 	)
