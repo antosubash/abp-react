ebc57a251803a210d9c6acef4c24d9c1f29b632f
Anto Subash <antosubash@live.com>
2024-04-14 23:26:17 +0200

    Update next.config.js

--- src/apps/abp/next.config.js
+++ src/apps/abp/next.config.js
@@ -4,10 +4,6 @@ module.exports = {
     reactStrictMode: true,
     transpilePackages: ['@abpreact/ui', '@abpreact/hooks', '@abpreact/utils', '@abpreact/proxy' ],
     output: 'standalone',
-    experimental: {
-        // this includes files from the monorepo base two directories up
-        outputFileTracingRoot: path.join(__dirname, '../../')
-    },
     images: {
         remotePatterns: [
             {
