3a9ff3ce8d55ca5ae9130d828e208388a93b60fb
Anto Subash <antosubash@live.com>
2024-06-07 15:48:49 +0200

    new turbo repo

--- v2/apps/web/src/app/page.tsx
+++ v2/apps/web/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
+import { Card } from "@abpreact/ui/card";
 
 function Gradient({
   conic,
--- v2/apps/web/tailwind.config.ts
+++ v2/apps/web/tailwind.config.ts
@@ -1,7 +1,7 @@
 // tailwind config is required for editor support
 
 import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import sharedConfig from "@abpreact/tailwind-config";
 
 const config: Pick<Config, "content" | "presets"> = {
   content: ["./src/app/**/*.tsx"],
