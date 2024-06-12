d44643506fa26cdda063ce683f398d6c49269386
Anto Subash <antosubash@live.com>
2024-06-12 15:52:25 +0200

    ```text chore: Update API route handling for GET, POST, PUT, and DELETE requests
    Refactor the API route handling in the code to improve consistency and readability. Update the GET, POST, PUT, and DELETE request functions to use the correct EXTERNAL_API_URL and include the necessary headers and body parameters. This change ensures that the API requests are properly authenticated and that the correct data is sent to the external API.

--- src/src/app/api/[...slug]/route.ts
+++ src/src/app/api/[...slug]/route.ts
@@ -1,36 +1,67 @@
 import { getSession } from '@/lib/session-utils';
 import { NextRequest, NextResponse } from 'next/server';
+
+const EXTERNAL_API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export async function GET(request: NextRequest) {
     const session = await getSession();
     const path = request.nextUrl.pathname;
-    const EXTERNAL_API_URL = process.env.NEXT_PUBLIC_API_URL;
-
     console.log(`${EXTERNAL_API_URL}${path}${request.nextUrl.search}`);
-
     try {
         return await fetch(`${EXTERNAL_API_URL}${path}${request.nextUrl.search}`, {
+            method: 'GET',
             headers: {
                 Authorization: `Bearer ${session.access_token}`,
-            },
-        });;
+                "__tenant": session.tenantId === 'default' ? undefined : session.tenantId
+            }  as any,
+        });
     } catch (e) {
         return NextResponse.json({ error: e }, { status: 500 });
     }
 }
 
 export async function POST(request: NextRequest) {
-    // reroute the request to the API
-    const res = new NextResponse();
     const session = await getSession();
-    const path = request.nextUrl.pathname.replace("/api/", "");
-    const EXTERNAL_API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-    return await fetch(`${EXTERNAL_API_URL}/${path}${request.nextUrl.search}`, {
+    const path = request.nextUrl.pathname;
+    return await fetch(`${EXTERNAL_API_URL}${path}${request.nextUrl.search}`, {
         method: 'POST',
         headers: {
             Authorization: `Bearer ${session.access_token}`,
             'Content-Type': 'application/json',
-        },
+            "__tenant": session.tenantId === 'default' ? undefined : session.tenantId
+        } as any,
         body: request.body,
+        duplex: "half"
+    } as any);
+}
+
+export async function PUT(request: NextRequest) {
+    const session = await getSession();
+    const path = request.nextUrl.pathname;
+    console.log(`${EXTERNAL_API_URL}${path}${request.nextUrl.search}`);
+    return await fetch(`${EXTERNAL_API_URL}${path}${request.nextUrl.search}`, {
+        method: 'PUT',
+        headers: {
+            Authorization: `Bearer ${session.access_token}`,
+            'Content-Type': 'application/json',
+            "__tenant": session.tenantId === 'default' ? undefined : session.tenantId
+        } as any,
+        body: request.body,
+        duplex: "half"
+    } as any);
+}
+
+export async function DELETE(request: NextRequest) {
+    // reroute the request to the API
+    const session = await getSession();
+    const path = request.nextUrl.pathname;
+    const url = `${EXTERNAL_API_URL}${path}${request.nextUrl.search}`;
+    return await fetch(url, {
+        method: 'DELETE',
+        headers: {
+            Authorization: `Bearer ${session.access_token}`,
+            'Content-Type': 'application/json',
+            "__tenant": session.tenantId === 'default' ? undefined : session.tenantId
+        } as any,
     });
 }
